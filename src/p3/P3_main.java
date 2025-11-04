ce11e5d569ff48154e358dc577f6b35d9ae87f99
Max <mvontrue@smail.uni-koeln.de>
2025-11-04 11:24:17 +0100

    Upload P3

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -6,14 +6,43 @@ public class P3_main {
 		
 		int a=-28;
 		
-		if(a<0)
+		if(a<0) {
 			
 			a=-1*a;
 		
-		System.out.println("Der Betrag von a ist " +a);
+		}
+	
+	 	System.out.println("Der Betrag von a ist " +a);
 		
+		
+		int b=4;
+		
+		System.out.println("Das Quadrat von b ist " +b*b);
+		
+		int c=6;
+		
+		if(c % 2 == 0) {
+			
+			System.out.println("c ist Gerade");
+		
+		}
+		
+			else {
+				
+				System.out.println("c ist Ungerade");
+			}
+		
+		
+		
+		
+		
+			
+			
+		}
 		// TODO Auto-generated method stub
 
 	}
+	
 
-}
+
+
